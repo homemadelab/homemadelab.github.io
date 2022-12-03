484116640217575b8a213cc1e7060057b49ebbff
Gaspar Pérez Mazás <covid19homelab@gmail.com>
2022-12-03 11:17:22 -0300

    Update app.js

--- coronavirus/es/farmacia/app.js
+++ coronavirus/es/farmacia/app.js
@@ -9,7 +9,7 @@ const respuesta4 = document.getElementById("respuesta4");
 const indiceRespuestaCorrecta = document.getElementById("indiceRespuestaCorrecta");
 const respuestas_span = [respuesta1, respuesta2, respuesta3, respuesta4];
 const tiempoRestante_span = document.getElementById("tiempoRestante");
-const fuente = document.getElementById("fuente");
+const fuente = document.getElementsByClassName("logo_fuente");
 
 function calcularTiempoRestante(tiempoRestante) {
   var i = 100;
