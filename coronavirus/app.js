83fe9b3d61657222c5978e879480b53af060d2dc
Gaspar Pérez Mazás <covid19homelab@gmail.com>
2022-12-07 21:25:49 -0300

    Testing

--- coronavirus/app.js
+++ coronavirus/app.js
@@ -48,7 +48,7 @@ function obtenerCategoria(id){
 }
 
 async function obtenerArchivo() {
-  const archivo = await fetch("/coronavirus/preguntas/" + id + ".csv"); // Recibo el archivo con las preguntas y respuestas
+  const archivo = await fetch("/coronavirus/preguntas/" + id.innerHTML + ".csv"); // Recibo el archivo con las preguntas y respuestas
   const datos = await archivo.text(); // Convierto el archivo a texto
   const tabla = datos.split("\n"); // Separo las preguntas
   return Promise.resolve(tabla);
