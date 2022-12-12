69a1872e4e2ff92cc5d6104883d3332dd8c35510
Gaspar Pérez Mazás <covid19homelab@gmail.com>
2022-12-12 20:12:29 -0300

    Agrego 5 preguntas de Zoológico en francés

--- coronavirus/app.js
+++ coronavirus/app.js
@@ -174,7 +174,7 @@ async function main() {
   var indiceRespuestasEscuela = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87];
   var indiceRespuestasBanco = [0, 3, 5, 8, 11, 14, 16, 19, 22, 24, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87];
   var indiceRespuestasLaboratorio = [0, 3, 5, 8, 11, 14, 16, 19, 22, 24, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87];
-  var indiceRespuestasZoologico = [0, 3, 5, 8, 11, 14, 16, 19, 22, 24, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87];
+  var indiceRespuestasZoologico = [0, 3, 6, 9, 12];
   var preguntaAleatoria = aleatorizarPreguntas();
   var j = 0;
 
