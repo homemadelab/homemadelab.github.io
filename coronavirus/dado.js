bfa8a766d7af28e5371dec55fb8316959810df34
Gaspar Pérez Mazás <covid19homelab@gmail.com>
2022-12-12 09:04:02 -0300

    Elimino un dado

--- coronavirus/dado.js
+++ coronavirus/dado.js
@@ -1,5 +1,4 @@
 var elDiceOne = document.getElementById('dice1');
-var elDiceTwo = document.getElementById('dice2');
 var elComeOut = document.getElementById('roll');
 
 elComeOut.onclick = function () {rollDice();};
@@ -7,21 +6,13 @@ elComeOut.onclick = function () {rollDice();};
 function rollDice() {
 
   var diceOne   = Math.floor((Math.random() * 6) + 1);
-  var diceTwo   = Math.floor((Math.random() * 6) + 1);
 
-  console.log(diceOne + ' ' + diceTwo);
+  console.log(diceOne);
 
   for (var i = 1; i <= 6; i++) {
     elDiceOne.classList.remove('show-' + i);
     if (diceOne === i) {
       elDiceOne.classList.add('show-' + i);
-    }
-  }
-
-  for (var k = 1; k <= 6; k++) {
-    elDiceTwo.classList.remove('show-' + k);
-    if (diceTwo === k) {
-      elDiceTwo.classList.add('show-' + k);
     }
   }
   setTimeout(rollDice(), 1000);
